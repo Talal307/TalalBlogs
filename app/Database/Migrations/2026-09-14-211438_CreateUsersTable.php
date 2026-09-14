eb31eda86a9e5230ff751a767ff9152750e34153
Talal307 <tahme228@gmail.com>
2026-09-15 00:21:17 +0300

    created users and password reset tokens migration

---
+++ app/Database/Migrations/2026-09-14-211438_CreateUsersTable.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateUsersTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'name' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '100',
+            ],
+            'email' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '191',
+                'unique'     => true,
+            ],
+            'password_hash' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '255',
+            ],
+            'bio' => [
+                'type' => 'TEXT',
+                'null' => true,
+            ],
+            'profile_image' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '255',
+                'null'       => true,
+            ],
+            'created_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+            'updated_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+        ]);
+
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('users');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('users');
+    }
+}
---
+++ app/Database/Migrations/2026-09-14-211521_CreatePasswordResetTokensTable.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreatePasswordResetTokensTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'email' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '191',
+            ],
+            'token' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '255',
+            ],
+            'created_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+            'expires_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+        ]);
+
+        $this->forge->addKey('id', true);
+        $this->forge->addKey('email');
+        $this->forge->createTable('password_reset_tokens');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('password_reset_tokens');
+    }
+}
